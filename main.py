1a542e885229a9b2685d37f7b7569317587c2c99
Ezequiel Medina <ezemed7@gmail.com>
2015-09-06 11:11:00 -0300

    primera idea de generacion de oraciones

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 __author__ = 'Ezequiel Medina'
 
 from MyCorpus import *
+from languagemodeling.ngram import NGram, NGramGenerator
 
 def main():
-    corpus = MyCorpus()
+    #corpus = MyCorpus()
     #lines = corpus.sents()
     #n = lines.__sizeof__()
 
-    print (corpus.sents2())
-    """try:
-        for s in corpus.sents():
-            print (s)
-    except IndexError:
-        #print ('no hay mas elementos')
-        pass"""
+    #print (corpus.sents2())
+
+    sents = [
+        'el gato come pescado .'.split(),
+        'la gata come salmón .'.split(),
+    ]
+    ngram = NGram(1, sents)
+    generator = NGramGenerator(ngram)
+    generator.generate_sent()
 
 
 if __name__=='__main__':
