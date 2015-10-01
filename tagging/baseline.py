c94799f89de8c9802d725ffc183d242bac5f40d4
Ezequiel Medina <ezemed7@gmail.com>
2015-10-01 12:34:03 -0300

    Clase BaselineTagger completa, pasa los tests

--- tagging/baseline.py
+++ tagging/baseline.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 
 
 class BaselineTagger:
@@ -6,7 +7,21 @@ class BaselineTagger:
         """
         tagged_sents -- training sentences, each one being a list of pairs.
         """
-        pass
+        self.words = defaultdict(int)
+        self.tagged_words = defaultdict(dict)
+        self.tags = defaultdict(int)
+        self.sents = defaultdict(int)
+        for sent in tagged_sents:
+            words = []
+            for s in sent:
+                self.words[s[0]] += 1
+                self.tags[s[1]] += 1
+                if s[1] not in self.tagged_words[s[0]]:
+                    self.tagged_words[s[0]][s[1]] = 0
+                self.tagged_words[s[0]][s[1]] += 1
+                words.append(s[0])
+            #self.sents[tuple(sent)] += 1
+            self.sents[tuple(words)] += 1
 
     def tag(self, sent):
         """Tag a sentence.
@@ -20,11 +35,33 @@ class BaselineTagger:
 
         w -- the word.
         """
-        return 'nc'
+        out = 'N'
+        tags = self.tagged_words[w]
+        tup = self.max_d(tags)
+        if tup != ():
+            out = tup[0]
+        return out
 
     def unknown(self, w):
         """Check if a word is unknown for the model.
 
         w -- the word.
         """
-        return True
+        out = False
+        tags = self.tagged_words[w]
+        tup = self.max_d(tags)
+        if tup == ():
+            out = True
+        return out
+
+    def max_d(self, d):
+        k = -1
+        m = 0
+        out = ()
+        for key in d:
+            if d[key] >= m:
+                k = key
+                m = d[key]
+        if k != -1:
+            out = tuple([k] + [m])
+        return out
