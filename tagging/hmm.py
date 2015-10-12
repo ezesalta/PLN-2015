33689c47d4a3d4885559f9c2dc1e519b56051d20
Ezequiel Medina <ezemed7@gmail.com>
2015-10-12 11:25:25 -0300

    clase ViterviTagger completa pero con errores, init de clase MLHMM

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -1,5 +1,6 @@
 __author__ = 'Ezequiel Medina'
 from math import log
+from collections import defaultdict
 
 
 class HMM:
@@ -12,9 +13,13 @@ class HMM:
         out -- output probabilities dictionary.
         """
         self.n = n
-        self.set_tags = tagset
         self.dict_trans = trans
         self.dict_out = out
+        self.set_tags = tagset
+        self.set_words = set()
+        for tag in out:
+            for w in out[tag].keys():
+                self.set_words.add(w)
 
     def tagset(self):
         """Returns the set of tags.
@@ -132,16 +137,118 @@ class HMM:
         return tagging
 
 
-
 class ViterbiTagger:
 
     def __init__(self, hmm):
         """
         hmm -- the HMM.
         """
+        self.hmm = hmm
+        self._pi = defaultdict(dict)
+        # self.backpointer = defaultdict(dict)
+        #self.prev_tags = ['<s>', '<s>']
+        #self._pi[0][tuple(self.prev_tags)] = 1
+        """self.tags_prob = defaultdict(dict)
+        tags = list(self.hmm.set_tags) + ['<s>']
+        for tag1 in tags:
+            for tag2 in self.hmm.set_tags:
+                print(tag2, tuple([tag1]))
+                self.tags_prob[tag1][tag2] = self.hmm.trans_prob(tag2, tuple([tag1]))
+        print(*self.tags_prob.items(), sep='\n')"""
 
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
 
         sent -- the sentence.
         """
+        # Init
+        tagging = []
+        init = ['<s>', '<s>']
+        prev_tags = init.copy()
+        self._pi[0][tuple(init)] = (1.0, tagging.copy())
+        # Recursive
+        #i = 0
+        #cant_trans = len(self.hmm.dict_trans)
+        #for word in sent:
+        for i in range(len(self.hmm.dict_trans)):
+            word = 'pepe'
+            if i < len(sent):
+                word = sent[i]
+            i += 1
+            # Asumo que siempre me da un tag
+            u = prev_tags[-1]
+            if u == '</s>':
+                break
+            aux = list(self.hmm.dict_trans[tuple(prev_tags)])
+            v = aux.pop(0)
+            if v == '</s>':
+                break
+            tagging.append(v)
+            p = [1.0]
+            if tuple(prev_tags) in self._pi[i - 1]:
+                p = self._pi[i - 1][tuple(prev_tags)]
+            # log?
+            q = self.hmm.trans_prob(v, tuple(prev_tags))
+            e = self.hmm.out_prob(word, v)
+            t = tuple([u] + [v])
+            #val = log(p[0] * q * e, 2)
+            val = p[0] * q * e
+            #print(i, 'p:', p, 'q:', q, 'e:', e, 't:', t, word, 'val:', val)
+            #tup = (val, tagging.copy())
+            #print(tup)
+            self._pi[i][t] = (val, tagging.copy())
+
+            # Set next prev_tags
+            if len(prev_tags) == 2:
+                prev_tags.pop(0)
+            prev_tags.append(v)
+
+        self._pi = dict(self._pi)
+        print(self._pi)
+
+        return tagging
+
+
+class MLHMM:
+
+    def __init__(self, n, tagged_sents, addone=True):
+        """
+        n -- order of the model.
+        tagged_sents -- training sentences, each one being a list of pairs.
+        addone -- whether to use addone smoothing (default: True).
+        """
+        self.counts_tag = defaultdict(int)
+        self.words = set()
+
+        for sent in tagged_sents:
+            sent_tags = []
+            for x in sent:
+                self.words.add(x[0])
+                sent_tags.append(x[1])
+
+            if n > 1:
+                sent_tags.insert(0, '<s>')
+            sent_tags.append('</s>')
+
+            for i in range(len(sent_tags) - n + 1):
+                ngram = tuple(sent_tags[i: i + n])
+                self.counts_tag[ngram] += 1
+                self.counts_tag[ngram[:-1]] += 1
+
+    def tcount(self, tokens):
+        """Count for an k-gram for k <= n.
+
+        tokens -- the k-gram tuple.
+        """
+        out = 0
+        if tokens in self.counts_tag:
+            out = self.counts_tag[tokens]
+        return out
+
+    def unknown(self, w):
+        """Check if a word is unknown for the model.
+
+        w -- the word.
+        """
+        return not (w in self.words)
+
