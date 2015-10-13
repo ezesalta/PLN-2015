d1d35340a14450fa9da7752d2e4382f062183bf5
Ezequiel Medina <ezemed7@gmail.com>
2015-10-13 10:51:54 -0300

    clase MLHMM completa, falta pasar un test. Algunos detalles en HMM

--- tagging/hmm.py
+++ tagging/hmm.py
@@ -145,16 +145,6 @@ class ViterbiTagger:
         """
         self.hmm = hmm
         self._pi = defaultdict(dict)
-        # self.backpointer = defaultdict(dict)
-        #self.prev_tags = ['<s>', '<s>']
-        #self._pi[0][tuple(self.prev_tags)] = 1
-        """self.tags_prob = defaultdict(dict)
-        tags = list(self.hmm.set_tags) + ['<s>']
-        for tag1 in tags:
-            for tag2 in self.hmm.set_tags:
-                print(tag2, tuple([tag1]))
-                self.tags_prob[tag1][tag2] = self.hmm.trans_prob(tag2, tuple([tag1]))
-        print(*self.tags_prob.items(), sep='\n')"""
 
     def tag(self, sent):
         """Returns the most probable tagging for a sentence.
@@ -163,7 +153,7 @@ class ViterbiTagger:
         """
         # Init
         tagging = []
-        init = ['<s>', '<s>']
+        init = ['<s>'] * (self.hmm.n - 1)
         prev_tags = init.copy()
         self._pi[0][tuple(init)] = (1.0, tagging.copy())
         # Recursive
@@ -180,6 +170,7 @@ class ViterbiTagger:
             if u == '</s>':
                 break
             aux = list(self.hmm.dict_trans[tuple(prev_tags)])
+            #if len(aux) > 0:
             v = aux.pop(0)
             if v == '</s>':
                 break
@@ -199,12 +190,12 @@ class ViterbiTagger:
             self._pi[i][t] = (val, tagging.copy())
 
             # Set next prev_tags
-            if len(prev_tags) == 2:
+            if len(prev_tags) == (self.hmm.n - 1):
                 prev_tags.pop(0)
             prev_tags.append(v)
 
         self._pi = dict(self._pi)
-        print(self._pi)
+        print(*self._pi.items(), sep='\n')
 
         return tagging
 
@@ -217,23 +208,50 @@ class MLHMM:
         tagged_sents -- training sentences, each one being a list of pairs.
         addone -- whether to use addone smoothing (default: True).
         """
+        self.counts = defaultdict(int)
         self.counts_tag = defaultdict(int)
         self.words = set()
+        self.tags = set()
+        self.addone = addone
+        self.n = n
+        self.dict_trans = defaultdict(dict)
 
-        for sent in tagged_sents:
-            sent_tags = []
-            for x in sent:
-                self.words.add(x[0])
-                sent_tags.append(x[1])
+        my_counts = defaultdict(int)
+        m = 1
+        while m <= n:
+            for sent in tagged_sents:
+                sent_tags = []
+                for x in sent:
+                    if m == 1:
+                        self.words.add(x[0])
+                        self.tags.add(x[1])
+                        self.counts[x] += 1
+                    sent_tags.append(x[1])
+                if m > 1:
+                    sent_tags.insert(0, '<s>')
+                sent_tags.append('</s>')
 
-            if n > 1:
-                sent_tags.insert(0, '<s>')
-            sent_tags.append('</s>')
-
-            for i in range(len(sent_tags) - n + 1):
-                ngram = tuple(sent_tags[i: i + n])
-                self.counts_tag[ngram] += 1
-                self.counts_tag[ngram[:-1]] += 1
+                for i in range(len(sent_tags) - m + 1):
+                    ngram = tuple(sent_tags[i: i + m])
+                    my_counts[ngram] += 1
+                    my_counts[ngram[:-1]] += 1
+            self.counts_tag.update(my_counts)
+            my_counts.clear()
+            m += 1
+        #print(*self.counts_tag.items(), sep='\n')
+        if n > 1:
+            prev_tags = ['<s>']*(n - 1)
+            for i in range(len(self.tagset())):
+                m = float('-inf')
+                t = '<s>'
+                for tag in self.tagset():
+                    p = self.trans_prob(tag, tuple(prev_tags))
+                    if p > m:
+                        m = p
+                        t = tag
+                self.dict_trans[tuple(prev_tags)][t] = m
+                prev_tags.pop(0)
+                prev_tags.append(t)
 
     def tcount(self, tokens):
         """Count for an k-gram for k <= n.
@@ -252,3 +270,124 @@ class MLHMM:
         """
         return not (w in self.words)
 
+    def count(self, tokens):
+        out = 0
+        if tokens in self.counts:
+            out = self.counts[tokens]
+        return out
+
+    def tagset(self):
+        """Returns the set of tags.
+        """
+        return self.tags
+
+    def trans_prob(self, tag, prev_tags):
+        """Probability of a tag.
+
+        tag -- the tag.
+        prev_tags -- tuple with the previous n-1 tags (optional only if n = 1).
+        """
+        out = 0
+        if prev_tags is None:
+            prev_tags = ()
+
+        tk = list(prev_tags) + [tag]
+        Ci = self.tcount(tuple(tk))
+        N = self.tcount(prev_tags)
+        if self.addone:
+            V = len(self.tags)
+            out = (Ci + 1) / float(N + V)
+        else:
+            if N > 0:
+                out = Ci / float(N)
+
+        #print('trans_prob', out, tag, prev_tags, self.addone)
+        return out
+
+    def out_prob(self, word, tag):
+        """Probability of a word given a tag.
+
+        word -- the word.
+        tag -- the tag.
+        """
+        out = 0
+        c1 = self.count((word, tag))
+        c2 = self.tcount((tag,))
+        if c2 > 0:
+            out = c1 / float(c2)
+
+        return out
+
+    # CONSULTAR!!!!
+    def tag_prob(self, y):
+        """
+        Probability of a tagging.
+        Warning: subject to underflow problems.
+
+        y -- tagging.
+        """
+        out = 1
+        yy = y.copy()
+        yy.append('</s>')
+        for i, tag in enumerate(yy):
+            prev_tags = ['<s>']
+            pos = i - (self.n - 1)
+            if pos >= 0:
+                prev_tags = yy[pos:i]
+            out *= self.trans_prob(tag, tuple(prev_tags))
+
+        return out
+
+    def tag_log_prob(self, y):
+        """
+        Log-probability of a tagging.
+
+        y -- tagging.
+        """
+        out = log(self.tag_prob(y), 2)
+
+        return out
+
+    def prob(self, x, y):
+        """
+        Joint probability of a sentence and its tagging.
+        Warning: subject to underflow problems.
+
+        x -- sentence.
+        y -- tagging.
+        """
+        out_prob = 1
+        for x in [self.out_prob(word, tag) for word, tag in zip(x, y)]:
+            out_prob *= x
+        trans_prob = self.tag_prob(y)
+
+        return out_prob * trans_prob
+
+    def log_prob(self, x, y):
+        """
+        Joint log-probability of a sentence and its tagging.
+
+        x -- sentence.
+        y -- tagging.
+        """
+        return log(self.prob(x, y), 2)
+
+    def tag(self, sent):
+        """Returns the most probable tagging for a sentence.
+
+        sent -- the sentence.
+        """
+        tagging = []
+        for word in sent:
+            max = 0
+            t = ''
+            for tag in self.set_tags:
+                prob = self.out_prob(word, tag)
+                if prob > max:
+                    max = prob
+                    t = tag
+            tagging.append(t)
+
+        # CONSULTAR !!!
+
+        return tagging
