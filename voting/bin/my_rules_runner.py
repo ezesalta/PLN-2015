e1e002399a234e039b412d568215b458a712b2d1
Ezequiel Medina <ezemed7@gmail.com>
2015-12-17 16:51:53 -0300

    detalles en my_rules_runner

--- voting/bin/my_rules_runner.py
+++ voting/bin/my_rules_runner.py
@@ -30,36 +30,43 @@ def run_from_command_line():
     logging.basicConfig(level=logging.INFO, format='%(message)s')
 
     try:
-        relation_name = iepy.instance.rules.RELATION
+        #relation_name = iepy.instance.rules.RELATION
+        pass
     except AttributeError:
         logging.error("RELATION not defined in rules file")
         sys.exit(1)
 
     try:
-        relation = models.Relation.objects.get(name=relation_name)
+        #relation = models.Relation.objects.get(name=relation_name)
+        relations = models.Relation.objects.all()
     except ObjectDoesNotExist:
-        logging.error("Relation {!r} not found".format(relation_name))
+        #logging.error("Relation {!r} not found".format(relation_name))
         sys.exit(1)
 
     # Load rules
-    rules = load_rules()
+    #rules = load_rules()
+    for relation in relations:
+        rules = [getattr(iepy.instance.rules, relation.name)]
+        if len(rules) == 0:
+            continue
 
-    # Load evidences
-    evidences = CandidateEvidenceManager.candidates_for_relation(relation)
+        # Load evidences
+        evidences = CandidateEvidenceManager.candidates_for_relation(relation)
 
-    # Run the pipeline
-    iextractor = RuleBasedCore(relation, rules)
-    iextractor.start()
-    iextractor.process()
-    predictions = iextractor.predict(evidences)
-    # Add my labeling
-    for pred in predictions:
-        if len(Label.objects.filter(evidence=pred, relation=relation)) == 0:
-            l = Label()
-            l.evidence = pred
-            l.label = predictions[pred]
-            l.relation = relation
-            l.save()
+        # Run the pipeline
+        iextractor = RuleBasedCore(relation, rules)
+        iextractor.start()
+        iextractor.process()
+        predictions = iextractor.predict(evidences)
+        # Save labeling
+        print('Saving labels...')
+        for pred in predictions:
+            if len(Label.objects.filter(evidence=pred, relation=relation)) == 0:
+                l = Label()
+                l.evidence = pred
+                l.label = predictions[pred]
+                l.relation = relation
+                l.save()
     #output.dump_output_loop(predictions)
 
 
--- voting/rules.py
+++ voting/rules.py
@@ -4,25 +4,17 @@
 from refo import Question, Star, Any, Plus
 from iepy.extraction.rules import rule, Token, Pos, Kind
 
-RELATION = "person_vote"
+RELATION = ""
 
 @rule(True)
-def begin_with_person(Subject, Object):
+def person_vote(Subject, Object):
     anything = Star(Any())
     #return Subject + anything + Object
     #return anything + Subject + anything + Object + anything
     return Subject + Kind("PARTY") + anything + Object
 
-@rule(True)
-def in_the_middle(Subject, Object):
-    anything = Star(Any())
-    return anything + Subject + Kind("PARTY") + anything + Object
-
-
-
-"""RELATION = "person_party"
 
 @rule(True)
-def from_party(Subject, Object):
+def person_party(Subject, Object):
     anything = Star(Any())
-    return Subject + Object"""
+    return Subject + Object
